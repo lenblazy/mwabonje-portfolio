13b1385a2f2a68fb196416b9a20d9778dc6ad16b
Lennox Mwabonje <lenblazy@gmail.com>
2025-07-02 19:04:26 +0300

    Add home page image

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import AppNav from "../AppNav";
 
 const Home = () => (
-	<AppNav />
+	<div>
+		<AppNav />
+		<section id="hero" className="hero section dark-background">
+			<img src="assets/img/home-bg.jpg" alt="" data-aos="fade-in" className="" />
+			<div className="container" data-aos="fade-up" data-aos-delay="100">
+				<h2>Lennox Mwabonje</h2>
+				<p>I'm <span className="typed" data-typed-items="Designer, Developer, Freelancer, Photographer">Designer</span><span className="typed-cursor typed-cursor--blink" aria-hidden="true"></span><span className="typed-cursor typed-cursor--blink" aria-hidden="true"></span></p>
+			</div>
+
+		</section>
+	</div>
+
 );
 
 export default Home;
