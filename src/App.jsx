08f1cf224d883849020387a3cd61d45454c3e252
Lennox Mwabonje <lenblazy@gmail.com>
2025-07-08 09:10:49 +0300

    Split code to pages and sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import PageWrapper from './components/PageWrapper';
 import Home from './components/pages/Home';
 
 const App = () => (
-	<PageWrapper />
-  );
+	<PageWrapper >
+		<Home />
+	</PageWrapper>
+);
 
 export default App;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import AppNav from "../AppNav";
+import Intro from "../sections/Intro";
+import About from "../sections/About";
+import Stats from "../sections/Stats";
+import Skills from "../sections/Skills";
+import Resume from "../sections/Resume";
+import Portfolio from "../sections/Portfolio";
 
 const Home = () => (
-	<section id="hero" className="hero section dark-background">
-		<img src="assets/img/home-bg.jpg" alt="" data-aos="fade-in" className="" />
-		<div className="container" data-aos="fade-up" data-aos-delay="100">
-			<h2>Lennox Mwabonje</h2>
-			<p>I'm a <span className="typed" data-typed-items="Designer, Developer, Freelancer, Photographer">Designer</span>
-				<span className="typed-cursor typed-cursor--blink" aria-hidden="true"></span>
-				<span className="typed-cursor typed-cursor--blink" aria-hidden="true"></span>
-			</p>
-			<div className="social-links">
-				<a href="#"><i className="bi bi-twitter-x"></i></a>
-				<a href="#"><i className="bi bi-facebook"></i></a>
-				<a href="#"><i className="bi bi-instagram"></i></a>
-				<a href="#"><i className="bi bi-linkedin"></i></a>
-			</div>
-		</div>
-
-	</section>
+	<div>
+		<AppNav />
+		<Intro />
+		<About />
+		<Stats />
+		<Skills />
+		<Resume />
+		<Portfolio />
+	</div>
 );
 
 export default Home;
