772e15bce90fa501cb3f255c650f633be2004267
Lennox Mwabonje <lenblazy@gmail.com>
2025-07-09 18:05:34 +0300

    Remove Useless div tag from Header component

--- src/ui/components/header/Header.jsx
+++ src/ui/components/header/Header.jsx
@@ -19,43 +19,41 @@ const Header = () => {
   }, []);
 
   return (
-    <div>
-      <header>
-        <div className="row">
-          <div className="top-bar">
-            <a
-              href="#"
-              className={`menu-toggle ${menuOpen ? "is-clicked" : ""}`}
-              onClick={handleToggleClick}
-            >
-              <span>Menu</span>
-            </a>
+    <header>
+      <div className="row">
+        <div className="top-bar">
+          <a
+            href="#"
+            className={`menu-toggle ${menuOpen ? "is-clicked" : ""}`}
+            onClick={handleToggleClick}
+          >
+            <span>Menu</span>
+          </a>
 
-            <div className="logo">
-              <a href="index.html">Lennox Mwabonje</a>
-            </div>
-
-            <nav id="main-nav-wrap">
-              <ul
-                className="main-navigation"
-                style={{ display: menuOpen ? "block" : "none" }}
-              >
-                <li className="current">
-                  <a className="smoothscroll" href="#intro" title="">
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a className="smoothscroll" href="#about" title="">
-                    About
-                  </a>
-                </li>
-              </ul>
-            </nav>
+          <div className="logo">
+            <a href="index.html">Lennox Mwabonje</a>
           </div>
+
+          <nav id="main-nav-wrap">
+            <ul
+              className="main-navigation"
+              style={{ display: menuOpen ? "block" : "none" }}
+            >
+              <li className="current">
+                <a className="smoothscroll" href="#intro" title="">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className="smoothscroll" href="#about" title="">
+                  About
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 };
 
